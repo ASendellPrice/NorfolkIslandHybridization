99ac624ca8dacbfbe0082813892726db9eae220d
ASendellPrice <ashley.sendell.price@gmail.com>
2023-01-20 12:45:36 +0000

    updated mapping script

--- scripts/03_read_mapping_novogene.sh
+++ scripts/03_read_mapping_novogene.sh
@@ -24,7 +24,7 @@ date > mapping/Zlat_2_Tgutt_ZW/read_mapping.log
 
 #For each of the novogene sequenced samples in resources/novogene_samples.txt
 #do the following:
-for SAMPLE in $(cat resources/novogene_samples.txt | head -n 1)
+for SAMPLE in $(cat resources/novogene_samples.txt)
 do
     #Make a temporary directory and move into it
     mkdir mapping_temp
@@ -41,34 +41,32 @@ do
         -R "@RG\tID:${BASE_NAME}\tSM:${SAMPLE}" \
         | samtools view -bS - \
         | samtools sort - > ${BASE_NAME}.bam
-    done
+    done    
 
     #If number of BAMs is greater than 1 then merge bams into a single file else
     #just rename the bam file
     if [ $(ls Filtered_${SAMPLE}*.bam | wc -l) -gt 1 ]
     then
         samtools merge ${SAMPLE}.bam Filtered_${SAMPLE}*.bam -f
-        rm Filtered_${SAMPLE}*.bam
     else
         mv Filtered_${SAMPLE}*.bam ${SAMPLE}.bam
     fi
 
     #Sort bam file and remove unsorted version
-    samtools sort ${SAMPLE}.bam > ../sample_bams/${SAMPLE}.sorted.bam
-    rm ${SAMPLE}.bam
+    samtools sort ${SAMPLE}.bam > ../mapping/Zlat_2_Tgutt_ZW/${SAMPLE}.sorted.bam
 
     #Index bam file
-    samtools index ../sample_bams/${SAMPLE}.sorted.bam
+    samtools index ../mapping/Zlat_2_Tgutt_ZW/${SAMPLE}.sorted.bam
 
     #Transfer full bam and index to s3 bucket
-    aws s3 cp ../sample_bams/${SAMPLE}.sorted.bam s3://norfolkhybrids/sample_bams/
-    aws s3 cp ../sample_bams/${SAMPLE}.sorted.bam.bai s3://norfolkhybrids/sample_bams/
+    ~/aws-cli/bin/aws s3 cp ../mapping/Zlat_2_Tgutt_ZW/${SAMPLE}.sorted.bam s3://norfolkhybrids/mapping/Zlat_2_Tgutt_ZW/
+    ~/aws-cli/bin/aws s3 cp ../mapping/Zlat_2_Tgutt_ZW/${SAMPLE}.sorted.bam.bai s3://norfolkhybrids/mapping/Zlat_2_Tgutt_ZW/
 
     #Tidy up
     cd ../
     rm -r mapping_temp
     
     #Add entry to log file
-    echo "mapping completed for sample" $SAMPLE "!" >> sample_bams/read_mapping.log
+    echo "mapping completed for sample" $SAMPLE "!" >> mapping/Zlat_2_Tgutt_ZW/read_mapping.log
 
 done
